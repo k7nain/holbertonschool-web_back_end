481260d59e1e59c60775efddc52f6e96dcc43788
k7nain <murslizadkenan@gmail.com>
2026-02-23 19:10:44 +0000

    Python - Variable Annotations Task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Module that contains function element_length"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+
+    """Returns a function length"""
     return [(i, len(i)) for i in lst]
