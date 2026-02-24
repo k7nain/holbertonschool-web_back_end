e0ab400f49c7d5934741ab9a86f1b2ff230e2651
k7nain <murslizadkenan@gmail.com>
2026-02-24 17:59:23 +0000

    Python - Async Comprehension Task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for async generator"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+
+    """Yields a random number between 0 and 10 every second, 10 times"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
