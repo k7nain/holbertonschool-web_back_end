8dc7fef505d0759095cc8767528350092d2bd327
k7nain <murslizadkenan@gmail.com>
2026-04-05 10:35:04 +0000

    i18n Task 1

---
+++ i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Flask app with Babel"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Home page"""
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
