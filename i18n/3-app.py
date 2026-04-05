3f8e0c95b56b139c3993754498be12e4fce7ed3b
k7nain <murslizadkenan@gmail.com>
2026-04-05 12:04:06 +0000

    i18n Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-class Config:
+class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
