18806528e7449737b2f4fbfc96cc0c2a90f4d618
k7nain <murslizadkenan@gmail.com>
2026-04-05 11:27:09 +0000

    i18n Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -23,7 +23,8 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+
+babel.init_app(app, locale_selector = get_locale)
 
 
 @app.route('/')
