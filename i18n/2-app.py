a3245214fc711de57f1b74e8462095a63f4c1bea
k7nain <murslizadkenan@gmail.com>
2026-04-05 10:46:57 +0000

    i18n Task 2

---
+++ i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Flask app with Babel and locale selection"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel()
+
+
+def get_locale():
+    """Determine best match language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """Home page"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
