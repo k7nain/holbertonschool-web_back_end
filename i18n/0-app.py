77cfe7c448678fc91d36bfa7c04faa63330a620d
k7nain <murslizadkenan@gmail.com>
2026-04-05 10:07:22 +0000

    i18n Task 0 edited

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+"""This module starts a Flask web application."""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+
+    """Display the home page."""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
