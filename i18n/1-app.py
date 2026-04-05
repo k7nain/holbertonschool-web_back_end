b088f3fba4e46bb1f634fd79b32068b952d51909
k7nain <murslizadkenan@gmail.com>
2026-04-05 10:37:24 +0000

    i18n Task 1

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -21,7 +21,7 @@ babel = Babel(app)
 @app.route('/')
 def index():
     """Home page"""
-    return render_template('index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
