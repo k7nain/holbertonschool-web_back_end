c5470d98ea12c250ebf6fb4a12e33d1e54cc8acb
k7nain <murslizadekenan@gmail.com>
2026-04-26 12:50:15 +0000

    Redis Basic Task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Cache module
+Cache module with Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -14,7 +14,22 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
-
         self._redis.set(key, data)
-
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        value = self._redis.get(key)
+
+        if value is None:
+            return None
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
