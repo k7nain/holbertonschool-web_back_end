3e63c1b9ced244d3b6f3d7919a4077154a32dee6
k7nain <murslizadekenan@gmail.com>
2026-04-26 11:08:08 +0000

    MySQL Advanced Task 1

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create a users table with country field
+-- if the table doesn't exists
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
